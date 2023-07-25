21db8bc2bb24dbdb8ab955048444ab7c59f9c2d5
aakrishtaa <116445696+aakrishtaa@users.noreply.github.com>
2023-07-25 16:47:06 +0400

    Create FifthGreatNinjaWar.java

---
+++ FifthGreatNinjaWar.java
@@ -0,0 +1,16 @@
+/**
+ * This is a concrete child class MistNinja which creates object for Ninja.
+ * @author aakrishtaa
+ * @version 11.0
+ */
+
+// creates two new arrays
+public class FifthGreatNinjaWar {
+    private Ninja[] firstTeam = new Ninja[5];
+    private Ninja[] secondTeam = new Ninja[5];
+
+/**
+     * the instance takes two params.
+     * @param firstTeam takes in the first array
+     * @param secondTeam takes in the second array
+     */
